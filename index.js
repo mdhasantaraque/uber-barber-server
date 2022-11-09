cfe56ef90d863a41e188dd46fe87d81e5cb2e7fd
Md. Hasan Taraque <mtaraque32@gmail.com>
2022-11-10 03:52:28 +0600

    my review api called

--- index.js
+++ index.js
@@ -38,6 +38,22 @@ async function run() {
       const result = await reviewCollection.insertOne(review);
       res.send(result);
     });
+
+    // review api
+    app.get("/reviews", async (req, res) => {
+      let query = {};
+
+      if (req.query.email) {
+        query = {
+          email: req.query.email,
+        };
+      }
+
+      const cursor = reviewCollection.find(query);
+      const reviews = await cursor.toArray();
+      res.send(reviews);
+    });
+
     app.get("/reviews/:id", async (req, res) => {
       const id = req.params.id;
       const query = { serviceId: id };
